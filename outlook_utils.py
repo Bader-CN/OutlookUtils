8fec8de9e13b9b5e696d35e8d3b8da96a081b1fd
Bader <zhanghong.personal@outlook.com>
2024-05-06 18:03:05 +0800

    Update outlook_utils.py

--- outlook_utils.py
+++ outlook_utils.py
@@ -210,6 +210,10 @@ def generate_sf_monthly_report(
     # 生成表头
     table.field_names = ["KPI", "{}-{}".format(str(y_offset), str(m_offset))]
 
+    # 设定需要共享的变量
+    open_cases_by_month = None
+    clos_cases_by_month = None
+
     # 处理 Case Report
     if len(case_list) > 0:
         try:
@@ -230,6 +234,11 @@ def generate_sf_monthly_report(
                     open_cases_m = open_cases_y[open_cases_y["Date/Time Opened"].dt.month == m_offset]
                     close_cases_y = rawcase[rawcase["Closed Date"].dt.year == y_offset]
                     close_cases_m = close_cases_y[close_cases_y["Closed Date"].dt.month == m_offset]
+                    # 赋值给共享变量, 这些值会用于其他的报表
+                    if len(open_cases_m) != 0:
+                        open_cases_by_month = open_cases_m
+                    if len(close_cases_m) != 0:
+                        clos_cases_by_month = close_cases_m
                     # 计算当前状态下状态为非 Closed 的 cases
                     backlog = rawcase[rawcase["Status"] != "Closed"]
                     backlog = backlog[backlog["Date/Time Opened"] <= pd.Timestamp(y_offset, m_offset, 1) + pd.offsets.MonthEnd()]
@@ -282,7 +291,6 @@ def generate_sf_monthly_report(
                         ssdata = pd.concat([ssdata1, ssdata2])
                         ssdata = ssdata.sort_values(by=["Suggested_Solution_Date"], ascending=False)
                         ssdata = ssdata.drop_duplicates(subset="Case ID")
-
                         if len(ssdata) != 0:
                             dtrdata = pd.DataFrame()
                             dtrdata["Date/Time Opened"] = pd.to_datetime(ssdata["Date/Time Opened"]).dt.date
@@ -292,6 +300,22 @@ def generate_sf_monthly_report(
                             summary_datas.append(["DTR", dtr_avg])
                         else:
                             summary_datas.append(["DTR", "-"])
+                        # DTR only Support
+                        ssdata1_os = backlog[backlog["Suggested_Solution_Date"].notna()]
+                        ssdata2_os = close_cases_m[close_cases_m["Suggested_Solution_Date"].notna()]
+                        ssdata_os = pd.concat([ssdata1_os, ssdata2_os])
+                        ssdata_os = ssdata_os.sort_values(by=["Suggested_Solution_Date"], ascending=False)
+                        ssdata_os = ssdata_os.drop_duplicates(subset="Case ID")
+                        ssdata_os = ssdata_os[ssdata_os["R&D Incident"].isna()]
+                        if len(ssdata_os) != 0:
+                            dtrdata_os = pd.DataFrame()
+                            dtrdata_os["Date/Time Opened"] = pd.to_datetime(ssdata_os["Date/Time Opened"]).dt.date
+                            dtrdata_os["Suggested_Solution_Date"] = pd.to_datetime(ssdata_os["Suggested_Solution_Date"]).dt.date
+                            dtr_os = dtrdata_os["Suggested_Solution_Date"] - dtrdata_os["Date/Time Opened"]
+                            dtr_os_avg = str(round(dtr_os.sum().days / len(ssdata_os), 2))
+                            summary_datas.append(["DTR Support only", dtr_os_avg])
+                        else:
+                            summary_datas.append(["DTR Support only", "-"])
                     else:
                         summary_datas.append(["Backlog > 30", "-"])
                         summary_datas.append(["Backlog > 30 (Support)", "-"])
@@ -307,6 +331,9 @@ def generate_sf_monthly_report(
                         summary_datas.append(["KCS Linkage", str(round(len(kcs_all) / len(close_cases_m) * 100, 2)) + "%"])
                     else:
                         summary_datas.append(["KCS Linkage", "-"])
+                    # Escalated
+                    if len(open_cases_m) != 0:
+                        summary_datas.append(["Escalated", str(len(open_cases_m[open_cases_m.Escalated != 0]))])
                     # 删除文件
                     try:
                         os.remove(raw_case_report_path)
@@ -337,13 +364,18 @@ def generate_sf_monthly_report(
                     survey_m = survey_y[survey_y["Closed Data"].dt.month == m_offset]
                     survey_ces = survey_m[survey_m["OpenText made it easy to handle my case"] >= 8.0]
                     survey_cast = survey_m[survey_m["Satisfied with support experience"] >= 8.0]
-                    # 分析数据并得出结果
+                    # Survey CES & Survey CAST
                     if len(survey_m) > 0:
                         summary_datas.append(["Survey CES", str(round(len(survey_ces) / len(survey_m) * 100, 2)) + "%"])
                         summary_datas.append(["Survey CAST", str(round(len(survey_cast) / len(survey_m) * 100, 2)) + "%"])
                     else:
                         summary_datas.append(["Survey CES", "-"])
                         summary_datas.append(["Survey CAST", "-"])
+                    # SRR (Survey Response Rate)
+                    if len(survey_m) > 0 and clos_cases_by_month is not None:
+                        summary_datas.append(["Survey Response Rate", str(round(len(survey_m) / len(clos_cases_by_month) * 100, 2)) + "%"])
+                    else:
+                        summary_datas.append(["Survey Response Rate", "-"])
                     # 删除文件
                     try:
                         os.remove(raw_surv_report_path)
